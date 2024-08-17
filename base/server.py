713f60eb86f85390a82b4be0a81683589cd9e9a9
amoxicilina1001 <felipecortesguzman@gmail.com>
2024-08-17 22:16:31 +0000

    Mundo guardado 2024-08-17

--- base/server.py
+++ base/server.py
@@ -14,10 +14,8 @@
 # in		      India (Mumbai)
 # jp		      Japón (Tokyo)
 # sa		      Sudamérica (São Paulo)
-# us		      Estados unidos (Ohio)
+# us		      Estados unidos (Ohio3)
 # us-cal-1	      Estados unidos (California)
-
-
 
 
 
